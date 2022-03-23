cf8f47f586025fe9c775382451adea260ffae055
sldjaakek12 <102232375+sldjaakek12@users.noreply.github.com>
2022-03-23 22:11:26 +0900

    Add files via upload

---
+++ d.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('안녕하세요')
+
+client.run('OTU0OTYwNDA1NDQ3NDUwNjg2.YjauLg.Qv1fQbMKpPkgKPw3QDnRi59EaJo')
+
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!!')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('안녕하세요')
+    
+client.run('OTU1NDI2MjMxMjY2OTY3NTcz.YjhgBA.OfOHxPgveeeCVuI1IcEejVC9UJY')
+
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!!!')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('안녕하세요')
+
+client.run('OTU1NDI1NzAzNzYxOTY1MTE3.Yjhfhg.6Dpy05csSsqXsBa66EUKKxlNy2A')
